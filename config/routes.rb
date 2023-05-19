058f7f0888652f1e687f3aff49a011bd74fb27ba
Muhammed <m.ali.arici01@gmail.com>
2023-05-19 11:28:53 +0300

    first heroku commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :keywords do
+    get "all", on: :collection
+  end
+
   # Defines the root path route ("/")
   # root "articles#index"
   root "keywords#index"
